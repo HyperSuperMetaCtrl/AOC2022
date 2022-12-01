87d9daa9c41ce967b1106ac2d6c46298ce15d23d
Marcel Wiesenhöfer <8881592+Unhexium@users.noreply.github.com>
2022-12-01 09:56:30 +0100

    add part 2

--- day1/src/main.rs
+++ day1/src/main.rs
@@ -20,4 +20,13 @@ fn main() {
         "Solution to day 1, part 1: {}",
         calories.iter().max().unwrap()
     );
+
+    calories.sort();
+    let mut acc = 0;
+
+    for c in calories.iter().rev().take(3) {
+        acc += c;
+    }
+
+    print!("Part 2: {}", acc);
 }
