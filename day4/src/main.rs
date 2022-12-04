fd12dc4c11a2467f7f3f3534c5f4ba4a1292a69c
Marcel Wiesenhöfer <8881592+HyperSuperMetaCtrl@users.noreply.github.com>
2022-12-04 11:33:22 +0100

    Add D4P2

--- day4/src/main.rs
+++ day4/src/main.rs
@@ -3,10 +3,10 @@ use anyhow::Result;
 
 const FILENAME: &str = "input.txt";
 
-fn transform(line: &str) -> (u32, u32, u32, u32) {
+fn transform(line: &str) -> (i32, i32, i32, i32) {
         let split: Vec<&str> = line.split(",").collect();
-        let lhs: Vec<u32> = split[0].split("-").map(|x| x.parse::<u32>().unwrap()).collect();
-        let rhs: Vec<u32> = split[1].split("-").map(|x| x.parse::<u32>().unwrap()).collect();
+        let lhs: Vec<i32> = split[0].split("-").map(|x| x.parse::<i32>().unwrap()).collect();
+        let rhs: Vec<i32> = split[1].split("-").map(|x| x.parse::<i32>().unwrap()).collect();
         (lhs[0],rhs[0],lhs[1],rhs[1])
 }
 
@@ -14,20 +14,27 @@ fn main() -> Result<()> {
     let args: Vec<String> = env::args().collect();
     let path = if args.len() >= 2 { &args[1] } else { FILENAME };
     
-    let intervals: Vec<(u32, u32, u32, u32)> = read_to_string(path)?
+    let intervals: Vec<(i32, i32, i32, i32)> = read_to_string(path)?
         .lines()
         .map(|line| transform(line))
         .collect();
 
-    let sum: u32 = intervals.iter().map(|interval| {
+    let sum: i32 = intervals.iter().map(|interval| {
         match interval {
             (l1,l2,r1,r2) if l1 <= l2 && r1 >= r2 => 1,
             (l1,l2,r1,r2) if l1 >= l2 && r1 <= r2 => 1,
             _ => 0,
         }
     }).sum();
-
     println!("Day 4 Part 1: {sum}");
+
+    let sum: i32 = intervals.iter().map(|interval| {
+        match interval {
+            (l1,l2,r1,r2) if std::cmp::max(l1,l2) <= std::cmp::min(r1,r2) => 1,
+            _ => 0,
+        }
+    }).sum();
+    println!("Day 4 Part 2: {sum}");
 
     Ok(())
 }
