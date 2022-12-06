9fb8212742668a0c78287ab462274422dc55606c
Marcel Wiesenhöfer <8881592+HyperSuperMetaCtrl@users.noreply.github.com>
2022-12-06 14:00:29 +0100

    Formatting

--- day5/src/main.rs
+++ day5/src/main.rs
@@ -1,10 +1,10 @@
 use std::error::Error;
 
-use anyhow:: Result;
+use anyhow::Result;
 use itertools::Itertools;
 use lazy_static::lazy_static;
 use regex::Regex;
-use std::{env, fs::read_to_string, fmt::Display};
+use std::{env, fmt::Display, fs::read_to_string};
 
 const FILENAME: &str = "input.txt";
 const NUM_STACKS: usize = 9;
@@ -15,17 +15,17 @@ struct Layer(Vec<MaybeCrate>);
 impl TryFrom<&str> for Layer {
     type Error = Box<dyn Error>;
     fn try_from(value: &str) -> Result<Self, Self::Error> {
-    lazy_static! {
-        //shoud fail at compile if Regex is not correct
-        static ref RE: Regex = Regex::new(r"(\[(?:[A-Z])\]|\s(?:\s)\s|\s(?:\d)\s)\s?").unwrap();
-    };
-    Ok(Layer(
-        RE.captures_iter(value)
-            .filter_map(|x| {
-                MaybeCrate::try_from(x[1].chars().nth(1).expect("Regex didn't match")).ok()
-            })
-            .collect(),
-    ))
+        lazy_static! {
+            //shoud fail at compile if Regex is not correct
+            static ref RE: Regex = Regex::new(r"(\[(?:[A-Z])\]|\s(?:\s)\s|\s(?:\d)\s)\s?").unwrap();
+        };
+        Ok(Layer(
+            RE.captures_iter(value)
+                .filter_map(|x| {
+                    MaybeCrate::try_from(x[1].chars().nth(1).expect("Regex didn't match")).ok()
+                })
+                .collect(),
+        ))
     }
 }
 
@@ -34,22 +34,22 @@ struct Layers(Vec<Layer>);
 
 impl Layers {
     fn stack(self) -> Stacks {
-    let mut v: Vec<Stack> = Vec::new();
-    for _ in 0..NUM_STACKS {
-        v.push(Stack(Vec::new()))
-    }
-    for layer in self.0.iter().rev() {
-        for (i, cr) in layer.0.iter().enumerate() {
-            if let MaybeCrate(Some(c)) = cr {
-                v[i].0.push(Crate(*c))
+        let mut v: Vec<Stack> = Vec::new();
+        for _ in 0..NUM_STACKS {
+            v.push(Stack(Vec::new()))
+        }
+        for layer in self.0.iter().rev() {
+            for (i, cr) in layer.0.iter().enumerate() {
+                if let MaybeCrate(Some(c)) = cr {
+                    v[i].0.push(Crate(*c))
+                }
             }
         }
-    }
-    Stacks(v)
+        Stacks(v)
     }
 }
 
-impl FromIterator<Layer> for Layers { 
+impl FromIterator<Layer> for Layers {
     fn from_iter<T: IntoIterator<Item = Layer>>(iter: T) -> Self {
         let mut v = Vec::new();
 
@@ -66,31 +66,31 @@ struct Stacks(Vec<Stack>);
 
 impl Stacks {
     fn rearrange(mut self, instructions: &Vec<Instruction>) -> Self {
-    for instruction in instructions {
-        let from = instruction.from;
-        let to = instruction.to;
-        for _ in 0..instruction.how_many {
-            let item = self.0[from].0.pop();
-            self.0[to].0.push(item.unwrap());
+        for instruction in instructions {
+            let from = instruction.from;
+            let to = instruction.to;
+            for _ in 0..instruction.how_many {
+                let item = self.0[from].0.pop();
+                self.0[to].0.push(item.unwrap());
+            }
         }
-    }
         self
     }
 
-fn rearrange_9001(mut self, instructions: &Vec<Instruction>) -> Self {
-    for instruction in instructions {
-        let from = instruction.from;
-        let to = instruction.to;
-        let mut temp = Vec::new();
-        for _ in 0..instruction.how_many {
-            temp.push(self.0[from].0.pop().unwrap());
+    fn rearrange_9001(mut self, instructions: &Vec<Instruction>) -> Self {
+        for instruction in instructions {
+            let from = instruction.from;
+            let to = instruction.to;
+            let mut temp = Vec::new();
+            for _ in 0..instruction.how_many {
+                temp.push(self.0[from].0.pop().unwrap());
+            }
+            for item in temp.into_iter().rev() {
+                self.0[to].0.push(item);
+            }
         }
-        for item in temp.into_iter().rev() {
-          self.0[to].0.push(item); 
-        }
-    } 
-    self
-}
+        self
+    }
 }
 impl Display for Stack {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
@@ -132,12 +132,12 @@ struct Instruction {
 impl TryFrom<&str> for Instruction {
     type Error = Box<dyn Error>;
     fn try_from(value: &str) -> Result<Self, Self::Error> {
-    let instr: Vec<&str> = value.split(" ").collect();
-    Ok(Instruction {
-        how_many: str::parse::<usize>(instr[1])?,
-        from: str::parse::<usize>(instr[3])? - 1, // -1 to convert from stack to vec index
-        to: str::parse::<usize>(instr[5])? - 1,
-    })
+        let instr: Vec<&str> = value.split(" ").collect();
+        Ok(Instruction {
+            how_many: str::parse::<usize>(instr[1])?,
+            from: str::parse::<usize>(instr[3])? - 1, // -1 to convert from stack to vec index
+            to: str::parse::<usize>(instr[5])? - 1,
+        })
     }
 }
 
@@ -172,7 +172,7 @@ fn main() -> Result<()> {
         .collect();
 
     let stacks = layers.stack();
-    
+
     let rearranged = stacks.clone().rearrange(&instructions);
     result(rearranged, 1);
 
