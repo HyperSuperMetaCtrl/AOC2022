326c7a280a85fd8a591d61e667c64d81d1db0a13
Marcel Wiesenhöfer <8881592+HyperSuperMetaCtrl@users.noreply.github.com>
2022-12-13 12:35:37 +0100

    Pull out assignment from breaking if
    border cases werent right without this

--- day8/src/main.rs
+++ day8/src/main.rs
@@ -4,6 +4,9 @@ use std::fs::read_to_string;
 use ndarray::prelude::*;
 use ndarray::Array2;
 
+const ROWS: usize = 99;
+const COLS: usize = 99;
+
 fn read_input_to_array() -> Result<(Array2<i32>, Array2<i32>)> {
     let input = read_to_string("input.txt")?;
     let input: Vec<Vec<i32>> = input
@@ -11,11 +14,8 @@ fn read_input_to_array() -> Result<(Array2<i32>, Array2<i32>)> {
         .map(|x| x.chars().map(|c| c.to_digit(10).unwrap() as i32).collect())
         .collect();
 
-    let rows = input.len();
-    let colums = input[0].len();
-
-    let mut array = Array2::<i32>::zeros((rows, colums));
-    let array1 = Array2::<i32>::zeros((rows, colums));
+    let mut array = Array2::<i32>::zeros((ROWS, COLS));
+    let array1 = Array2::<i32>::zeros((ROWS, COLS));
 
     for (i, row) in input.iter().enumerate() {
         for (j, column) in row.iter().enumerate() {
@@ -44,6 +44,45 @@ fn main() -> Result<()> {
     }
     let sum = array1.sum();
     println!("Day 8 part1: {}", sum);
+    let mut scores = Vec::new();
+    for (idx, elem) in array.indexed_iter() {
+        let slice_left = array.slice(s![..idx.0;-1, idx.1]);
+        let slice_right = array.slice(s![(idx.0 + 1).., idx.1]);
+        let slice_up = array.slice(s![idx.0, ..idx.1;-1]);
+        let slice_down = array.slice(s![idx.0, (idx.1 + 1)..]);
+
+        let mut right = 0;
+        let mut left = 0;
+        let mut up = 0;
+        let mut down = 0;
+        for (i, other) in slice_right.iter().enumerate() {
+            right = i + 1;
+            if elem <= other {
+                break;
+            }
+        }
+        for (i, other) in slice_left.iter().enumerate() {
+            left = i + 1;
+            if elem <= other {
+                break;
+            }
+        }
+        for (i, other) in slice_up.iter().enumerate() {
+            up = i + 1;
+            if elem <= other {
+                break;
+            }
+        }
+        for (i, other) in slice_down.iter().enumerate() {
+            down = i + 1;
+            if elem <= other {
+                break;
+            }
+        }
+        scores.push(left * right * up * down);
+    }
+    dbg!(&scores);
+    println!("Day 8 part 2: {}", scores.iter().max().unwrap());
 
     Ok(())
 }
