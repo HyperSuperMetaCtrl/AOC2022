90fe2c19d0236c7f99cda310bdc7e1d1b6826288
Marcel Wiesenhöfer <8881592+HyperSuperMetaCtrl@users.noreply.github.com>
2022-12-20 20:26:45 +0100

    WIP Day13

--- day13/day13.hs
+++ day13/day13.hs
@@ -1,4 +1,31 @@
-com1 [] [] = True
+import System.IO( isEOF )
+main = do
+  res <- mainloop 1 []
+  print . sum $ res
+
+
+getpair = do
+  end <- isEOF
+  if end
+    then do
+    return ("f","f")
+    else do
+    a <- getLine
+    b <- getLine
+    empty <- getLine
+    return (a, b)
+
+mainloop index list = do
+  a <- getpair
+  if a == ("f","f") then return list
+    else
+    if uncurry com1 a
+    then do mainloop (index+1) (index:list)
+    else do mainloop (index+1) list
+
+com1 [] [] = False
+com1  _ [] = True
+com1 [] _ = False
 -- When list starts with 10 convert it to char ':' (ASCII-value = '9'+1)
 com1 ('1':'0':xs) y = com1 (':':xs) y
 com1 x ('1':'0':ys) = com1 x (':':ys)
@@ -7,8 +34,8 @@ com1 ('[':xs) ('[':ys) = com1 xs ys
 com1 (']':xs) (']':ys) = com1 xs ys
 com1 (',':xs) (',':ys) = com1 xs ys
 -- if one list runs out before the other cases
-com1 (']':xs) (y:ys)    = True
-com1 (x:xs)   (']':ys)  = False
+com1 (']':xs) y         = True
+com1 x        (']':ys)  = False
 -- wrap single item into a list for comparison
 com1 x@('[':xs) (y:',':ys)  = com1 x              ('[':y:']':ys)
 com1 x@('[':xs) (y:']':ys)  = com1 x              ('[':y:']':ys)
@@ -22,4 +49,3 @@ com1 (a:xs) (b:ys)
   where right = a < b
         wrong = b < a
         continue = com1 xs ys
-
