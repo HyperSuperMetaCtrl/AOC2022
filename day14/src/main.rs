1629b7a1c2ee976817376299a899604eecb24209
Marcel Wiesenhöfer <8881592+HyperSuperMetaCtrl@users.noreply.github.com>
2022-12-21 23:48:51 +0100

    WIP

--- day14/src/main.rs
+++ day14/src/main.rs
@@ -1,12 +1,14 @@
 use anyhow::Result;
 use itertools::Itertools;
+use ndarray::prelude::*;
 use ndarray::Array2;
 
+type Point = (isize, isize);
 #[derive(Debug)]
 struct Shape {
-    coordinates: Vec<(usize, usize)>,
+    coordinates: Vec<Point>,
 }
-
+#[derive(Debug, Clone)]
 enum Material {
     Air,
     Solid,
@@ -15,41 +17,85 @@ enum Material {
 
 struct CaveSystem {
     map: Array2<Material>,
-    spawn: (usize, usize),
+    spawn: Point,
+}
+impl CaveSystem {
+    fn new(shape: (usize, usize), spawn: Point) -> Self {
+        let map = Array2::<Material>::from_shape_simple_fn(shape, || Material::Air);
+        Self { map, spawn }
+    }
+    fn draw_structures(&mut self, shapes: Vec<Shape>) {
+        for shape in shapes {
+            for segment in shape.coordinates.windows(2) {
+                let leftx = std::cmp::min(segment[0].0, segment[1].0);
+                let rightx = std::cmp::max(segment[0].0, segment[1].0);
+                let lefty = std::cmp::min(segment[0].1, segment[1].1);
+                let righty = std::cmp::max(segment[0].1, segment[1].1);
+                self.map
+                    .slice_mut(s![leftx..=rightx, lefty..=righty])
+                    .fill(Material::Solid);
+            }
+        }
+    }
+    fn simulate_sand(&mut self) {}
 }
 
 fn parse_input(path: &str) -> Result<Vec<Shape>> {
     let input = std::fs::read_to_string(path)?;
+    // split into lines
     let lines: Vec<&str> = input.lines().collect();
+    // split into single points as &str
     let splits: Vec<Vec<&str>> = lines
         .into_iter()
         .map(|line| line.split("->").collect::<Vec<&str>>())
         .collect();
+    // trim whitespace
     let trimmed: Vec<Vec<&str>> = splits
         .into_iter()
         .map(|x| x.into_iter().map(|y| y.trim()).collect())
         .collect();
     let tuples: Vec<Shape> = trimmed
         .into_iter()
+        //create Shapes from Vec<Vec<&str>>
         .map(|shape| Shape {
             coordinates: shape
                 .into_iter()
                 .map(|item| {
+                    // split and parse into isize
                     item.split(",")
                         .collect::<Vec<&str>>()
                         .into_iter()
-                        .map(|item| item.parse::<usize>().unwrap())
+                        .map(|item| item.parse::<isize>().unwrap())
                         .collect_tuple()
                         .unwrap()
                 })
-                .collect::<Vec<(usize, usize)>>(),
+                .collect::<Vec<Point>>(),
         })
         .collect();
     Ok(tuples)
 }
 
 fn main() -> Result<()> {
-    let parsed_input = parse_input("input.txt");
-    dbg!(parsed_input);
+    let parsed_input = parse_input("input.txt")?;
+
+    let max_x = parsed_input
+        .iter()
+        .map(|x| x.coordinates.iter().map(|x| x.0).max().unwrap())
+        .max()
+        .unwrap()
+        + 1;
+    let max_y = parsed_input
+        .iter()
+        .map(|x| x.coordinates.iter().map(|x| x.1).max().unwrap())
+        .max()
+        .unwrap()
+        + 1;
+    println!("maxx: {}, maxy: {}", max_x, max_y);
+
+    let spawn = (500, 0);
+    let mut cave = CaveSystem::new((max_x as usize, max_y as usize), spawn);
+    cave.draw_structures(parsed_input);
+    println!("{:?}", cave.map);
+    //dbg!(cave.map);
     Ok(())
 }
